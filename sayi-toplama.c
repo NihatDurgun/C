bbdb5c4d9eea32d2738e6839eef9a0061a41bd62
Emre Durgun <nihattdurgun@gmail.com>
2018-11-02 12:49:11 +0300

    basit matematiksel işlemler

---
+++ sayi-toplama.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int sayi1=10;
+    int sayi2=20;
+    int sonuc = sayi1+sayi2;
+
+    printf("1.Sayi ve 2.Sayi Toplami: %d + %d = %d",sayi1,sayi2,sonuc);
+
+
+}
