a9a53cc2d83c6dab40e1582db85e98676d15ad93
Emre Durgun <nihattdurgun@gmail.com>
2018-11-02 13:05:17 +0300

    "*" karakteriyle piramit yapımı

---
+++ piramit.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+
+void main(){
+
+     for(int i = 1;i<=5;i++){
+        for(int x = 1;x<=i;x++){
+            printf("*");
+        }
+        printf("\n");
+     }
+
+
+
+}
+
+/*
+
+*
+**
+***
+****
+*****
+
+*/
