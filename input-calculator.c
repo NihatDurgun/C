bcab41cf6d1711593b0a89881f0068d5ee39d6d1
Emre Durgun <nihattdurgun@gmail.com>
2018-11-02 12:49:33 +0300

    Kullanıcıdan değer alma

---
+++ input-calculator.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+    int s1,s2; //Sayıları tanımlıyoruz
+
+    printf("1.Sayiyi Giriniz: ");
+    scanf("%d",&s1); //1.Girdi Alıyoruz
+    printf("2.Sayiyi Giriniz: ");+
+    scanf("%d",&s2); //2.Girdi Alıyoruz
+    printf("Islem Sonucu: %d",(s1+s2));
+
+
+
+
+}
