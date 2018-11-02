64d017c1d8a05aac22da26323e8519b7bfeabe6a
Emre Durgun <nihattdurgun@gmail.com>
2018-11-02 12:53:27 +0300

    Daire Alan Hesaplama

---
+++ hesaplayici.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    float yaricap,pi=3.14;
+    printf("Hosgeldiniz!\nLutfen Yaricap Giriniz: ");
+    scanf("%f",&yaricap);
+
+    printf("Dairenin Alani:%f",(pi*yaricap*yaricap));
+    printf("\nDairenin Cevresi: %f",(2*pi*yaricap));
+
+
+
+
+
+}
