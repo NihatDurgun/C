f0af40401c2662dcaa3905705a9593abd4161f89
Nihat Durgun <nihattdurgun@gmail.com>
2019-03-06 10:41:41 +0300

    Matrix Transpozesi

---
+++ main(1).c
@@ -0,0 +1,68 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    //matrixin transpozesini almak! 
+    
+    int a[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+    
+    printf("Duzenlenmemiş Matrix: ");
+    
+    int x,y;
+    printf("{");
+    for(x=0;x<3;x++){
+        printf("{");
+        for(y=0;y<3;y++){
+            if(y!= 2)   
+            printf("%d,",a[x][y]); 
+            else
+            printf("%d",a[x][y]); 
+        }
+        if(x!= 2)
+        printf("},");
+        else
+        printf("}");
+    }
+    printf("}\n\n");
+    
+    
+    int temp=0;
+    for(x=0;x<3;x++){
+        
+        for(y=0;y<x;y++){
+            temp = a[x][y];
+            a[x][y] = a[y][x];
+            a[y][x] = temp;
+            
+        } 
+        
+    }
+    
+    
+    printf("Transpoze Edilmiş Matrix: ");
+    
+    printf("{");
+    for(x=0;x<3;x++){
+        printf("{");
+        for(y=0;y<3;y++){
+            if(y!= 2)   
+            printf("%d,",a[x][y]); 
+            else
+            printf("%d",a[x][y]); 
+        }
+        if(x!= 2)
+        printf("},");
+        else
+        printf("}");
+    }
+    printf("}");
+    
+}
